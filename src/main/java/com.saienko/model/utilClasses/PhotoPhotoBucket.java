e0eb898d4041e0d9aa136e8b78fa4a10a109350a
gleb <glebachkawr2@gmail.com>
2015-12-05 22:30:46 +0200

    ...

---
+++ src/main/java/com.saienko/model/utilClasses/PhotoPhotoBucket.java
@@ -0,0 +1,29 @@
+package com.saienko.model.utilClasses;
+
+import com.saienko.model.Photo;
+import com.saienko.model.PhotoBucket;
+
+/**
+ * Created by gleb on 03.12.2015.
+ */
+public class PhotoPhotoBucket {
+
+    private Photo photo = new Photo();
+    private PhotoBucket photoBucket = new PhotoBucket();
+
+    public Photo getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Photo photo) {
+        this.photo = photo;
+    }
+
+    public PhotoBucket getPhotoBucket() {
+        return photoBucket;
+    }
+
+    public void setPhotoBucket(PhotoBucket photoBucket) {
+        this.photoBucket = photoBucket;
+    }
+}
